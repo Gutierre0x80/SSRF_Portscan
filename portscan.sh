c619dcce0bcdd496405d66c5235b871fbd190190
Matheus Gutierre <63872706+Gutierre0x80@users.noreply.github.com>
2023-06-23 12:06:46 -0300

    Update portscan.sh

--- portscan.sh
+++ portscan.sh
@@ -19,7 +19,7 @@ echo "                              ^----|   /          |^"
 echo "                                  ^|  |___________|^"
 echo "                                  ^|  | ((_(_)| )_)^"
 echo "                                  ^|  \_((_(_)|/(_)^"
-echo "                                  ^\______________(^"
+echo "                                  ^\______________)^"
 echo ""
 echo -e " \033[1;31m Project created by Gutierre0x80 \033[0m"
 echo "" 
@@ -36,7 +36,7 @@ if [ "$3" == "--top-ports" ]; then
         	echo "Testing port: $i"
         	curl $1$2=http://localhost:$i
 	done
-	echo"Scan Completed in Top Ports mode" 
+	echo "Scan Completed in Top Ports mode" 
 	exit 1;
 
 elif [ "$3" == "--all-ports" ]; then
@@ -46,7 +46,7 @@ elif [ "$3" == "--all-ports" ]; then
                 echo "Testing port: $i"
                 curl $1$2=http://localhost:$i
         done
-	echo"Scan Completed in All Ports mode" 
+	echo "Scan Completed in All Ports mode" 
 	exit 1;
 
 else
